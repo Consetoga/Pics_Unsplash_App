70cd5f1f7c6d50f20f1b6f859edff43fcfe72697
Conestoga <exceldeveloper3@gmail.com>
2022-12-02 11:32:05 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,81 @@
+const bodyParser = require("body-parser");
+const { response } = require("express");
+const express = require("express");
+
+const app     = express();
+
+
+
+app.use(bodyParser.urlencoded({extended:true}))
+const request = require("request");
+
+app.listen(2000,()=>{
+
+  console.log("App is running at port 2000!");
+
+});
+
+app.post("/pics/",(req,res)=>{
+
+  const data = req.body
+
+  const pgno = data.pgnum
+  
+  request("https://api.unsplash.com/photos?client_id=RFXSb4ctQK3zc2f-awkk3HsyTxDdq3G6YH2Ip0_nwX0&page="+pgno,(error,response,body)=>{
+
+
+
+
+    if(error){
+        console.log(error);
+    }
+   
+    else{
+      const data = JSON.parse(body);
+     
+      res.render("pics.ejs",{
+
+        mypics : data
+      });
+ 
+    }
+ 
+ 
+ 
+ });
+
+});
+
+
+app.get("/pics/:pgNum",(req,res)=>{
+
+     const pgNumber = req.params.pgNum ;
+
+     console.log(pgNumber);
+
+    request("https://api.unsplash.com/photos?client_id=RFXSb4ctQK3zc2f-awkk3HsyTxDdq3G6YH2Ip0_nwX0&page="+pgNumber,(error,response,body)=>{
+
+
+
+
+        if(error){
+            console.log(error);
+        }
+       
+        else{
+          const data = JSON.parse(body);
+         
+          res.render("pics.ejs",{
+
+            mypics : data
+          });
+     
+        }
+     
+     
+     
+     });
+
+});
+
+
